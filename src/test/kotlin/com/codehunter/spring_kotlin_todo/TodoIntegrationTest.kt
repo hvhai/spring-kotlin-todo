257adfd667ec44b54298fce5cbe4f6f8db7463ee
hvhai <hunterx6323@gmail.com>
2025-01-06 16:45:45 +0700

    test: add controller test and integration test

--- src/test/kotlin/com/codehunter/spring_kotlin_todo/TodoIntegrationTest.kt
+++ src/test/kotlin/com/codehunter/spring_kotlin_todo/TodoIntegrationTest.kt
@@ -1,9 +1,15 @@
 package com.codehunter.spring_kotlin_todo
 
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -12,7 +18,9 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 abstract class ContainerBaseTest {
     companion object {
@@ -46,13 +54,165 @@ class TodoIntegrationTest : ContainerBaseTest() {
     @Autowired
     lateinit var todoRepository: TodoRepository
 
-    @Test
-    fun `given valid data when create note called then return new note`() {
-        val actual = restTemplate.postForEntity<Todo>("/api/todos", CreateNoteRequest("new note"), Todo::class.java)
-        assertNotNull(actual)
-
-        val allNotes = todoRepository.findAll()
-        assertEquals(1, allNotes.size)
+    @AfterEach
+    fun tearDown() {
+        todoRepository.deleteAll()
     }
 
+    @Test
+    fun `given valid data when create note called then return new note`() {
+        // when
+        val actual: ResponseEntity<ResponseDTO<Todo>> =
+            restTemplate.exchange(
+                "/api/todos",
+                HttpMethod.POST,
+                HttpEntity(CreateNoteRequest("new note")),
+                typeReference<ResponseDTO<Todo>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(201), actual.statusCode)
+
+        val newTodo = actual.body?.data
+        assertNotNull(newTodo?.id)
+        assertEquals("new note", newTodo.note)
+        assertFalse(newTodo.isDone)
+
+        // verify database
+        val allNotes = todoRepository.findAll()
+        assertEquals(1, allNotes.size)
+        val newTodoEntity = allNotes.get(0)
+        assertNotNull(newTodoEntity?.id)
+        assertEquals("new note", newTodoEntity.note)
+        assertFalse(newTodoEntity.isDone)
+    }
+
+    @Test
+    fun `given existent note when get note by id then return note`() {
+        // given
+        val existentNote = todoRepository.save<TodoEntity>(TodoEntity(null, "note", true))
+        val id = existentNote.id
+        val expectedNote = Todo(id!!, "note", true)
+
+        // when
+        val actual: ResponseEntity<ResponseDTO<Todo>> =
+            restTemplate.exchange(
+                "/api/todos/$id",
+                HttpMethod.GET,
+                null,
+                typeReference<ResponseDTO<Todo>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(200), actual.statusCode)
+        val newTodo = actual.body?.data
+        assertEquals(expectedNote, newTodo)
+    }
+
+    @Test
+    fun `given 2 existent notes when get all notes then return 2 notes`() {
+        // given
+        val notes = todoRepository.saveAll<TodoEntity>(
+            listOf<TodoEntity>(
+                TodoEntity(null, "note1", true),
+                TodoEntity(null, "note2", false),
+            )
+        )
+        // when
+        val actual: ResponseEntity<ResponseDTO<List<Todo>>> =
+            restTemplate.exchange(
+                "/api/todos",
+                HttpMethod.GET,
+                null,
+                typeReference<ResponseDTO<List<Todo>>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(200), actual.statusCode)
+        val newTodo = actual.body?.data
+        assertEquals(notes.map { it.toDomain() }, newTodo)
+    }
+
+    @Test
+    fun `given existent note when update note by id then return updated note`() {
+        // given
+        val existentNote = todoRepository.save<TodoEntity>(TodoEntity(null, "note", true))
+        val id = existentNote.id
+        val expectedNote = Todo(id!!, "updated note", true)
+
+        // when
+        val actual: ResponseEntity<ResponseDTO<Todo>> =
+            restTemplate.exchange(
+                "/api/todos",
+                HttpMethod.PATCH,
+                HttpEntity(UpdateNoteRequest(id, "updated note")),
+                typeReference<ResponseDTO<Todo>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(200), actual.statusCode)
+        val newTodo = actual.body?.data
+        assertEquals(expectedNote, newTodo)
+
+        // verify database
+        val updatedNoteEntity = todoRepository.findByIdOrNull(id)
+        assertNotNull(updatedNoteEntity)
+        assertEquals(TodoEntity(id, "updated note", true), updatedNoteEntity)
+    }
+
+    @Test
+    fun `given existent note when mark not as done by id then return updated note`() {
+        // given note with isDone = false
+        val existentNote = todoRepository.save<TodoEntity>(TodoEntity(null, "note", false))
+        val id = existentNote.id
+        val expectedNote = Todo(id!!, "note", true)
+
+        // when
+        val actual: ResponseEntity<ResponseDTO<Todo>> =
+            restTemplate.exchange(
+                "/api/todos/$id/done",
+                HttpMethod.PATCH,
+                null,
+                typeReference<ResponseDTO<Todo>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(200), actual.statusCode)
+        val newTodo = actual.body?.data
+        assertEquals(expectedNote, newTodo)
+
+        // verify database
+        val updatedNoteEntity = todoRepository.findByIdOrNull(id)
+        assertNotNull(updatedNoteEntity)
+        assertEquals(TodoEntity(id, "note", true), updatedNoteEntity)
+    }
+
+    @Test
+    fun `given existent note when delete note by id then note is deleted with empty response`() {
+        // given
+        val existentNote = todoRepository.save<TodoEntity>(TodoEntity(null, "note", true))
+        val id = existentNote.id
+
+        // when
+        val actual: ResponseEntity<ResponseDTO<Todo>> =
+            restTemplate.exchange(
+                "/api/todos/$id",
+                HttpMethod.DELETE,
+                null,
+                typeReference<ResponseDTO<Todo>>()
+            )
+        // then
+        // verify response
+        assertNotNull(actual)
+        assertEquals(HttpStatusCode.valueOf(204), actual.statusCode)
+
+        // verify database
+        val updatedNoteEntity = todoRepository.findByIdOrNull(id)
+        assertNull(updatedNoteEntity)
+    }
 }
